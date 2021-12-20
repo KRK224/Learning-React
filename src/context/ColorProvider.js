25ddebb0a2e77b0b97e2e58ec5b9f40d9b4be6f5
KRyun <hihas93@naver.com>
2021-12-20 11:31:41 +0900

    ColorProvider 커스텀 프로바이더를 설정하여 state 변경

---
+++ src/context/ColorProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState} from 'react';
+import { ColorContext } from '..';
+import colorData from '../data/color-data.json';
+
+const colorContext = createContext();
+
+export default function ColorProvider ({children}) {
+  const [colors, setColors] = useState(colorData);
+
+  return (
+    <ColorContext.Provider value={{colors, setColors}}>
+      {children}
+    </ColorContext.Provider>
+  );
+};
