a4388525087b461e5f8d3a537a48a249a8427df6
KRyun <hihas93@naver.com>
2021-12-20 15:10:17 +0900

    StarRating 오탈자 수정

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -15,7 +15,7 @@ export default function StarRating({
       {createArray(totalStars).map((n,i) => 
         <Star 
           key={i} 
-          selected={selectedStars > i }s
+          selected={selectedStars > i }
           onSelect={()=> onRate(i+1)}
         />)}
       <p>
